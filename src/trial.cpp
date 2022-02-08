8223d28a5e093b699256cfd3a04d1d143ac4480c
rxndom266 <ankita266.behera@gmail.com>
2022-02-08 22:18:17 +0530

    .
    changed map to set

--- src/trial.cpp
+++ src/trial.cpp
@@ -1,35 +1,71 @@
 #include <iostream>
-#include <map>
+#include <set>
 #include <regex>
 using namespace std;
+const set<string> keys = {
+        "if",
+        "elif",
+        "for",
+        "in",
+        "while",
+        "do",
+        "return",
+        "break",
+        "continue",
+        "select",
+        "case",
+        "default",
+        "def",
+        "global",
+        "static",
+        "const",
+        "int",
+        "float",
+        "char",
+        "bool",
+        "string",
+        "true",
+        "false",
+        "void"
+    };
+const set<string> delims = {
+            "{",
+            "}",
+            ";",
+            ",",
+            "[",
+            "]",
+            "(",
+            ")",
+            ":",
+            "?",
+            "-",
+            "*",
+            "/",
+            "%",
+            ">",
+            ">=",
+            "<",
+            "<=",
+            "!=",
+            "&&",
+            "||",
+            "!",
+            "&",
+            "|",
+            "<<",
+            ">>",
+            "^",
+            "--",
+            "~",
+            "=",
+            "+=",
+            "-=",
+            "=>"
+        };
 
 bool isKeyword(string s){
-    map<string, string> keys = {
-        {"if", "if"},
-        {"elif", "elif"},
-        {"else", "else"},
-        {"for", "for"},
-        {"in", "in"},
-        {"while", "while"},
-        {"do", "do"},
-        {"return", "return"},
-        {"break", "break"},
-        {"continue", "continue"},
-        {"select", "select"},
-        {"case", "case"},
-        {"default", "default"},
-        {"def", "def"},
-        {"global", "global"},
-        {"static", "static"},
-        {"const", "const"},
-        {"int", "int"},
-        {"float", "float"},
-        {"char", "char"},
-        {"bool", "bool"},
-        {"string", "string"},
-        {"true", "true"},
-        {"true", "true"}
-    };
+    
     if(keys.find(s)!=keys.end())
         return true;
     else
@@ -41,41 +77,7 @@ bool isDelim(string s){
     if(regex_match(s, regex(blank)))
         return true;
     else{
-        map<string, string> delims = {
-            {"{", "{"},
-            {"}", "}"},
-            {";", ";"},
-            {",", ","},
-            {"[", "["},
-            {"]", "]"},
-            {"(", "("},
-            {")", ")"},
-            {":", ":"},
-            {"?", "?"},
-            {"-", "-"},
-            {"*", "*"},
-            {"/", "/"},
-            {"%", "%"},
-            {">", ">"},
-            {">=", ">="},
-            {"<", "<"},
-            {"<=", "<="},
-            {"!=", "!="},
-            {"&&", "&&"},
-            {"||", "||"},
-            {"!", "!"},
-            {"&", "&"},
-            {"|", "|"},
-            {"<<", "<<"},
-            {">>", ">>"},
-            {"^", "^"},
-            {"--", "--"},
-            {"~", "~"},
-            {"=", "="},
-            {"+=", "+="},
-            {"-=", "-="},
-            {"=>", "=>"}
-        };
+        
         if(delims.find(s)!=delims.end())
             return true;
         else
@@ -85,7 +87,7 @@ bool isDelim(string s){
 int main(int argc, char** argv){
     string s;
     getline(cin, s);
-    cout<<"*"<<s<<"*"<<"\n";
+    //cout<<"*"<<s<<"*"<<"\n";
     cout << isDelim(s);
     return 0;
 }
